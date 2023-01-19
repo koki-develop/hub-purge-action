f28bb3ea3fbb66737c04c238a3863b2485804ba0
koki-develop <kou.pg.0131@gmail.com>
2023-01-20 08:43:51 +0900

    get default branch

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const core = require("@actions/core");
+const github = require("@actions/github");
 
-try {
-  console.log("Hello World, 1");
-} catch (error) {
+const repo = "koki-develop";
+const owner = "koki-develop";
+
+const main = async () => {
+  const idToken = core.getIDToken();
+  const octo = github.getOctokit(idToken);
+  const { data } = await octo.rest.repos.get({
+    repo,
+    owner,
+  });
+  const branch = data.default_branch;
+
+  console.log(`branch: ${branch}`);
+};
+
+main().catch((error) => {
   core.setFailed(error.message);
-}
+});
