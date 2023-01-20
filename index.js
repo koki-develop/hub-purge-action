9358a98759117358cc1147ce3e557ac5da7421d4
koki-develop <kou.pg.0131@gmail.com>
2023-01-20 09:22:52 +0900

    list camo urls

--- index.js
+++ index.js
@@ -1,19 +1,72 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-
-const repo = "koki-develop";
-const owner = "koki-develop";
+const axios = require("axios").default;
+const cheerio = require("cheerio");
+const path = require("path");
 
 const main = async () => {
-  const githubToken = core.getInput("token");
-  const octo = github.getOctokit(githubToken);
-  const { data } = await octo.rest.repos.get({
-    repo,
-    owner,
-  });
-  const branch = data.default_branch;
+  /*
+   * inputs
+   */
 
-  console.log(`branch: ${branch}`);
+  // repository
+  const [owner, repo] = (() => {
+    const input = core.getInput("repository");
+    if (!input) {
+      return [github.context.repo.owner, github.context.repo.repo];
+    }
+    return input.split("/");
+  })();
+
+  // paths
+  const paths = (core.getInput("paths") || "README.md").trim().split("\n");
+
+  // token
+  const octokit = github.getOctokit(core.getInput("token"));
+
+  // branch
+  const branch = await (async () => {
+    const branch = core.getInput("branch");
+    if (branch) {
+      return branch;
+    }
+
+    // get default branch
+    const { data: repoData } = await octokit.rest.repos.get({ owner, repo });
+    return repoData.default_branch;
+  })();
+
+  /*
+   * action
+   */
+
+  for (const p of paths) {
+    // get html document
+    const docUrl = new URL(`https://github.com`);
+    docUrl.pathname = path.join(
+      docUrl.pathname,
+      owner,
+      repo,
+      "blob",
+      branch,
+      p
+    );
+    const { data: html } = await axios.get(docUrl.href);
+    const $ = cheerio.load(html);
+
+    // get camo urls
+    const urls = $("img[src^='https://camo.githubusercontent.com/']").map(
+      (_, img) => {
+        $(img).attr("src");
+      }
+    );
+
+    // clear caches
+    for (const url of urls) {
+      // TODO: clear
+      console.log(url);
+    }
+  }
 };
 
 main().catch((error) => {
